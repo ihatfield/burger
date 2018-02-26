f51015dcb2226a049b93ded1052b83c74724c649
Isla Hatfield <30755706+ihatfield@users.noreply.github.com>
2018-02-26 12:19:10 -0700

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers(burger_name, finished)
+VALUES ("Big Mac", false),
+  ("Whopper", false),
+  ("Cheeseburger", false),
+  ("Baconator", false),
+  ("Qt Pounder", false),
+  ("BLT", false),
+  ("Veggie", true);
