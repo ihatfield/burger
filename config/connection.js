805b3a66dc2b4676116b165f4571444c56cb85ee
Isla <hatfieldisla@gmail.com>
2018-02-26 13:26:54 -0700

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
 var mysql = require('mysql');
 var connection;
 
-// For Heroku Deployment vs. Local MySQL Database
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else{
-  connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root', 
-    database : 'burgers_db' 
-  });
+} else {
+  const connection = mysql.createConnection({
+      port: 3306,
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'burgers_db'
+  })
 }
 
 connection.connect
